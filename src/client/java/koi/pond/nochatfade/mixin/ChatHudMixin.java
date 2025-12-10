23bf8f4e81ac21b773dfe8e0c1b0193c28b66879
Koi <koipond.minecraft@gmail.com>
2025-12-10 17:58:25 -0500

    release 1.0 for 1.21.11

--- src/client/java/koi/pond/nochatfade/mixin/ChatHudMixin.java
+++ src/client/java/koi/pond/nochatfade/mixin/ChatHudMixin.java
@@ -1,30 +1,34 @@
 package koi.pond.nochatfade.mixin;
 
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.ChatHud.OpacityRule;
 //import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ChatHud.class)
-public class ChatHudMixin {
+@Mixin(ChatHud.OpacityRule.class)
+public interface ChatHudMixin {
     /**
      * Disables any chat fading behavior
      */
-    @Inject(method = "getMessageOpacityMultiplier", at = @At("HEAD"), cancellable = true)
-    private static void getMessageOpacityMultiplier(int age, CallbackInfoReturnable<Double> cir) {
+    @Inject(method = "timeBased", at = @At("HEAD"), cancellable = true)
+    private static void timeBased(int currentTick, CallbackInfoReturnable<OpacityRule> cir) {
         /* Vanilla behavior
-        double d = (double)age / (double)200.0F;
-        d = (double)1.0F - d;
-        d *= (double)10.0F;
-        d = MathHelper.clamp(d, (double)0.0F, (double)1.0F);
-        d *= d;
-         */
+        return (line) -> {
+            int j = currentTick - line.addedTime();
+            double d = (double)j / (double)200.0F;
+            d = (double)1.0F - d;
+            d *= (double)10.0F;
+            d = MathHelper.clamp(d, (double)0.0F, (double)1.0F);
+            d *= d;
+            return (float)d;
+        };
+        */
 
-        double d = 1.0D; //Always force full opacity
-
-        cir.setReturnValue(d);
-        //return d;
+        //Instead of calculating opacity based on time/tick, ignore time and force full visibility always
+        //(The "line" object is: net.minecraft.client.gui.hud.ChatHudLine.Visible line)
+        cir.setReturnValue((line) -> 1.0F);
     }
 }
